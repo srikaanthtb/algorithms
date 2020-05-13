44bc04d6397da9b06b8c0fda9e35bacef18573fa
srikaanthtb <srikaanth.t.b@gmail.com>
2020-05-13 15:21:57 +0100

    write 3 algorithms

--- lib/timing.rb
+++ lib/timing.rb
@@ -1,10 +1,39 @@
-def timing
+def shufflesort(c)
      stime = Time.now
-    a = (1..50000000).to_a.shuffle
+    a = (1..c).to_a.shuffle
     etime = Time.now
 
     total = etime - stime
-    puts total
+    puts "time taken for shuffle sort " + total.to_s + " seconds"
 end
 
-timing()
+
+def reversesort(c)
+     stime = Time.now
+    a = (1..c).to_a.reverse
+    etime = Time.now
+
+    total = etime - stime
+    puts "time taken for reverse sort " + total.to_s + " seconds"
+end
+def lastsort(c)
+     stime = Time.now
+    a = (1..c).to_a.last
+    etime = Time.now
+
+    total = etime - stime
+    puts " time taken for last sort " + total.to_s + " seconds"
+end
+
+x = 0
+while x < 500000000
+reversesort(x)
+
+print " value of x is: " + x.to_s
+x += 1
+end
+
+
+
+# shufflesort(50000000)
+# lastsort(50000000)
